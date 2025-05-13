a6dd7762ca10bd32ee6d09630da83037cd19b7ea
Mikolaj Kos <mkk3contact@gmail.com>
2025-05-13 17:27:57 +0200

    reformat code with black

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Module: utils
 Contains basic calculator functions for addition, subtraction, multiplication, and division.
 """
 
+
 def add(a: int, b: int) -> int:
     """Return the sum of a and b."""
     return a + b
